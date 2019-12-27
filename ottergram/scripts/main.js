c8e55323773171a7bfd24c17979f7f573c425b0d
aqm1970 <aqmontgomery@gmail.com>
2019-12-27 10:54:52 -0500

    Hide and show the detail image

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -32,20 +34,43 @@ function addThumbClickHandler(thumbnail) {
   thumbnail.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumbnail);
+    showDetails();
   });
 }
 
 function getThumbnailsArray() {
-  'uss strict';
+  'use strict';
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
 }
 
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
